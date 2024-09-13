74ab3fc51ea8821560d399a73fadb60233e352a2
uo284226 <uo284226@uniovi.es>
2024-09-13 19:17:38 +0200

    Segundo commit

--- src/main/java/es/tew/HolaMundoServlet.java
+++ src/main/java/es/tew/HolaMundoServlet.java
@@ -10,6 +10,8 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @WebServlet("/HolaMundo")
 
+// Hola
+
 public class HolaMundoServlet extends HttpServlet{
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
