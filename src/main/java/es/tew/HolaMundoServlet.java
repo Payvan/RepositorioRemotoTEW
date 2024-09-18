66fe168493dac90baa4c80e7534e44d78e6f78de
uo284226 <uo284226@uniovi.es>
2024-09-18 17:08:56 +0200

    2.1-Servlets con parametros

--- src/main/java/es/tew/HolaMundoServlet.java
+++ src/main/java/es/tew/HolaMundoServlet.java
@@ -1,6 +1,8 @@
 package es.tew;
+
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Vector;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -12,17 +14,21 @@ import jakarta.servlet.http.HttpServletResponse;
 
 // Hola
 
-public class HolaMundoServlet extends HttpServlet{
+public class HolaMundoServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        
+
+        String nombre = (String) req.getParameter("NombreUsuario");
         resp.setCharacterEncoding("UTF-8");
         resp.setContentType("text/html");
         PrintWriter out = resp.getWriter();
         out.println("<HTML>");
         out.println("<HEAD><TITLE>Hola Mundo!</TITLE></HEAD>");
         out.println("<BODY>");
-        out.println("Bienvenido a mi primera página web!");
+        if (nombre != null) {
+            out.println("<br>Hola " + nombre + "<br>");
+        }
+        out.println("Bienvenido a mi primera página Web!");
         out.println("</BODY></HTML>");
 
     }
